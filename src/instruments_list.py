91b5408fd7bae198e75621ad300585a182ba8f46
Liang Yi <liangyi2012@gmail.com>
2021-02-12 11:07:53 +0800

    update auth, instruments_list

--- src/instruments_list.py
+++ src/instruments_list.py
@@ -6,9 +6,25 @@ import oandapyV20
 import oandapyV20.endpoints.accounts as accounts
 from exampleauth import exampleAuth
 
-accountID, token = exampleAuth()
-client = oandapyV20.API(access_token=token)
 
-r = accounts.AccountInstruments(accountID=accountID)
-rv = client.request(r)
-print(json.dumps(rv, indent=2))
+def print_instruments(api: oandapyV20.API, acc: str):
+    r = accounts.AccountInstruments(accountID=acc)
+    rv = api.request(r)
+    print(json.dumps(rv, indent=2))
+
+
+def print_summary(api: oandapyV20.API, acc: str):
+    r = accounts.AccountSummary(accountID=acc)
+    rv = api.request(r)
+    print(json.dumps(rv, indent=2))
+
+
+def main():
+    accountID, token = exampleAuth()
+    client = oandapyV20.API(access_token=token)
+    # print_instruments(client, accountID)
+    print_summary(client, accountID)
+
+
+if __name__ == '__main__':
+    main()
